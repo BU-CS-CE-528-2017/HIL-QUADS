3e330d97d04dc8bcad7af7902730c91098d62a61
saacg <isaacg@bu.edu>
2017-02-21 08:23:17 -0500

    adding script that quickly adds switches to hil - for API testing

---
+++ development/add_switches.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# simple script to quickly add switches in hil for API testing
+
+haas switch_register mockswitch18 mock switchhost01 switchuser01 password1234
+haas switch_register mockswitch36 mock switchhost02 switchuser02 password1234
+haas switch_register mockswitch72 mock switchhost02 switchuser02 password1234
+haas switch_register mockswitch54 mock switchhost03 switchuser03 password1234
+haas switch_register mockswitch613 mock switchhost05 switchuser04 password1234
